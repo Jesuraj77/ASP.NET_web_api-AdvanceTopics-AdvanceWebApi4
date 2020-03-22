9a6fdfefeb4f63be3e0a4a86d06784c76a2a3833
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 14:02:53 +0530

    code added

---
+++ AdvanceWebApi4/Controllers/AdvanceFeaturesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceWebApi4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdvanceFeaturesController : ControllerBase
+    {
+        //code here to Perform Dependency injection and register the services using Add scoped and Add transient options.
+
+        //Show how does Etag works while reading and update records and implement caching using Entity tags. 
+
+        // code here to Implement versioning using URL.
+
+
+    }
+}
